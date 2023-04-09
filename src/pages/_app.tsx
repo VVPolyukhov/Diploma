d8a518befdf4bb51cc23267029c340dba1f99bf0
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-04-09 11:53:00 +0300

    Пофиксил билд

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import Layout from "components/Layout";
 import { wrapper } from "store";
 import 'antd/dist/reset.css';
-import 'antd/dist/antd.css';
 import "styles/index.scss";
 
 const App: React.FC<AppProps> = ({ Component, ...rest }) => {
