57c5cfc8cb45be5b6471f2eb28ef90e618094725
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-04-09 13:22:49 +0300

    Попытка исправить загрузку CSS для antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import Layout from "components/Layout";
 import { wrapper } from "store";
-import 'antd/dist/reset.css';
+// import "antd/dist/reset.css";
 import "styles/index.scss";
 
 const App: React.FC<AppProps> = ({ Component, ...rest }) => {
@@ -11,7 +11,7 @@ const App: React.FC<AppProps> = ({ Component, ...rest }) => {
   return (
     <Provider store={store}>
       <Layout>
-        <Component  {...props.pageProps} />
+        <Component {...props.pageProps} />
       </Layout>
     </Provider>
   );
