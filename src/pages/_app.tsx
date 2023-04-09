01d75f40415a5cb7934cec3c236f4ad9464f2ba1
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-04-09 14:14:26 +0300

    Вернул импорты обратно

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import Layout from "components/Layout";
 import { wrapper } from "store";
-// import "antd/dist/reset.css";
+import "antd/dist/reset.css";
 import "styles/index.scss";
 
 const App: React.FC<AppProps> = ({ Component, ...rest }) => {
