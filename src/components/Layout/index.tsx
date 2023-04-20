cdf745db53224a60ce8b522b61821821b05a3b45
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-04-20 10:57:36 +0300

    Попытка изобразить лендинг

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,50 @@
+import { DesktopOutlined } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
 import React, { PropsWithChildren } from "react";
 
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: any
+) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+  getItem("Option 1", "1", <DesktopOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <DesktopOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <DesktopOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <DesktopOutlined />),
+];
+
 interface IProps {}
-const Layout: React.FC<PropsWithChildren<IProps>> = ({ children }) => {
-  return <>{children}</>;
+const { Header, Content, Sider } = Layout;
+const MainLayout: React.FC<PropsWithChildren<IProps>> = ({ children }) => {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider theme="light" collapsible>
+        <Menu items={items} />
+      </Sider>
+      <Layout>
+        <Header />
+        <Content>{children}</Content>
+      </Layout>
+    </Layout>
+  );
 };
 
-export default Layout;
+export default MainLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,18 @@ import Layout from "components/Layout";
 import { wrapper } from "store";
 import "antd/dist/reset.css";
 import "styles/index.scss";
+import { useEffect } from "react";
 
+/**
+ * При первой загрузке приложения необходимо получить accessToken, если есть refreshToken
+ * Если запрос получится неудачным, то просто отрисовываем кнопку "Войти", иначе появляется профиль пользователя
+ * 
+ * Для публичных страниц:
+ * Загружаем по SSR контент для SEO, проверки по авторизации могут быть на клиенте
+ * 
+ * Для приватных страниц:
+ * ??? Выключаем SSR, так как индексации страницы происходить не будет, авторизация через клиент
+*/
 const App: React.FC<AppProps> = ({ Component, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
 
