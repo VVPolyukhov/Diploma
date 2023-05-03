22584b1d306a2b29c229a2ce234d72b87be06787
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-05-04 00:33:34 +0300

    Секция "О нас" на главной странице

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,7 @@ export default function Home() {
       </Head>
       <section className={styles.preview}>
         <div className={styles.leftSide}>
-          <div className={styles.tag}>
-            InCourse - образовательная платформа
-          </div>
+          <div className={styles.tag}>InCourse - образовательная платформа</div>
           <div className={styles.additionalText}>
             <span className={styles.marked}>Курсы маркетига</span>
             <br />
@@ -26,6 +24,45 @@ export default function Home() {
         <div className={styles.rightSide}>
           <div className={styles.image}>
             <Image alt="" src={previewImage} fill />
+          </div>
+        </div>
+      </section>
+      <section className={styles.aboutUs}>
+        <h1 className={styles.title}>О нас</h1>
+        <span className={styles.text}>
+          InCourse - образовательная платформа с самыми современными курсами
+          маркетинга в социальных сетях от проверенных медийных экспертов
+        </span>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <div className={styles.primaryTitle}>Польза</div>
+            <h3 className={styles.secondaryTitle}>Статьи и уроки</h3>
+            <span className={styles.description}>
+              Изучай бесплатные материалы сразу на платформе
+            </span>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.primaryTitle}>Нетворкинг</div>
+            <h3 className={styles.secondaryTitle}>Тематические мероприятия</h3>
+            <span className={styles.description}>
+              Находи коллег, партнёров и заказчиков, посещая нетворкинг
+              мероприятия на платформе
+            </span>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.primaryTitle}>Поддержка</div>
+            <h3 className={styles.secondaryTitle}>За руку с экспертом</h3>
+            <span className={styles.description}>
+              Пройди свой путь к большим заказам вместе с экспертом
+            </span>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.primaryTitle}>Выбор</div>
+            <h3 className={styles.secondaryTitle}>На одной волне</h3>
+            <span className={styles.description}>
+              Изучай статьи, знакомься с экспертами выбирай преподавателя по
+              душе
+            </span>
           </div>
         </div>
       </section>
