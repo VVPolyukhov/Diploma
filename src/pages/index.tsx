3b8d38fc3df7f660e98329da72d6ba823d7ca37b
VVPolyukhov <vlad.polyukhov@gmail.com>
2023-05-09 20:21:46 +0300

    Тест useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { useAuth } from "utils/hooks/useAuth";
 import PrivateHome from "components/tree/Home/Private";
 import PublicHome from "components/tree/Home/Public";
+import { useEffect } from "react";
 
 export default function Home() {
   const isAuth = useAuth();
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
 
   if (isAuth) {
     return <PrivateHome />;
