3f1770ce40dff7815f761b3c25cd0891b6f1b8ef
Полякова Елена <lenapolukova6729@gmail.com>
2023-05-23 22:51:51 +0300

    Фикс 2

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,8 +2,7 @@ import PrivateHome from 'components/tree/Main';
 import { NextPage } from 'next';
 
 const MainPage: NextPage = () => {
-  //
   return <PrivateHome />;
-}
+};
 
 export default MainPage
