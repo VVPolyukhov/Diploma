f899a24660703e8a30e65ca623f6d236a49ae455
Полякова Елена <lenapolukova6729@gmail.com>
2023-05-30 21:41:39 +0300

    Верстка карточки нетворкинг-мероприятия

--- src/components/tree/Main/index.tsx
+++ src/components/tree/Main/index.tsx
@@ -1,8 +1,11 @@
+import Button from "components/kit/Button";
 import CommonTag from "components/shared/CommonTag";
 import ReducedArticlesList from "components/tree/Articles/List/Reduced";
 import ReducedCoursesList from "components/tree/Courses/Reduced";
 import React from "react";
-import styles from './index.module.scss';
+import network1 from "images/Network1.png";
+import styles from "./index.module.scss";
+import Image from "next/image";
 
 interface IProps {}
 const PrivateHome: React.FC<IProps> = () => {
@@ -11,10 +14,29 @@ const PrivateHome: React.FC<IProps> = () => {
       {/* TODO: Collapse antd */}
       <CommonTag>Статьи</CommonTag>
       <ReducedArticlesList />
-      
+
       {/* TODO: Collapse antd */}
       <CommonTag>Курсы</CommonTag>
       <ReducedCoursesList />
+
+      <CommonTag>Нетворкинг-мероприятия</CommonTag>
+      <div className={styles.events}>
+        <div className={styles.event}>
+          <div className={styles.content}>
+            <h1>SMM</h1>
+            <h3>6 июня в 18:00 по МСК </h3>
+            <div>
+              <h5>Осталось 10/20 мест</h5>
+              <Button type="primary" className={styles.btn}>
+                Зарегистрироваться
+              </Button>
+            </div>
+          </div>
+          <div className={styles.img}>
+            <Image src={network1} alt="" width={300} height={300} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
