f6967b84f98f5b62112bc0de0dd85a77d64d59ba
Полюхов Владислав <vlad.polyukhov@gmail.com>
2023-06-03 11:14:55 +0300

    Фикс

--- src/store/api/utils.ts
+++ src/store/api/utils.ts
@@ -7,7 +7,6 @@ import { BaseQueryFn, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { notification } from "antd";
 import { Mutex } from "async-mutex";
 import { IAuthBaseResponse } from "store/auth/types";
-import { TRootState } from "store/types";
 import {
   getRefreshToken,
   setRefreshToken,
@@ -19,12 +18,8 @@ import {
 
 const mutex = new Mutex();
 
-export const prepareHeaders: FetchBaseQueryArgs["prepareHeaders"] = (
-  headers,
-  { getState }
-) => {
-  const state = getState() as TRootState;
-  const accessToken = getAccessToken(); // getAccessToken(state);
+export const prepareHeaders: FetchBaseQueryArgs["prepareHeaders"] = (headers) => {
+  const accessToken = getAccessToken();
   if (accessToken) {
     headers.set("Authorization", `Bearer ${accessToken}`);
   }
@@ -33,7 +28,7 @@ export const prepareHeaders: FetchBaseQueryArgs["prepareHeaders"] = (
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: `http${
-    process.env.NODE_ENV === "development" ? "" : "s"
+    process.env.NODE_ENV === "development" ? "" : "" // : "s"
   }://84.252.73.203:8080/api/v1/`,
   prepareHeaders,
 });
@@ -47,57 +42,54 @@ export const baseQueryWithReauth: BaseQueryFn<
   // await mutex.waitForUnlock();
   let result = await baseQuery(args, api, extraOptions);
   if (result.error) {
-    notification.error({ message: "Произошла непредвиденная ошибка" });
+    if (result.error.status === 401) {
+      // checking whether the mutex is locked
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
+        try {
+          const refreshToken = getRefreshToken();
+
+          if (refreshToken) {
+            const refreshResult = await baseQuery(
+              {
+                url: "/auth/token",
+                body: {
+                  refreshToken,
+                },
+              },
+              api,
+              extraOptions
+            );
+            if (refreshResult.data) {
+              setRefreshToken(
+                (refreshResult.data as IAuthBaseResponse).refreshToken
+              );
+              api.dispatch(
+                setAccessToken(
+                  (refreshResult.data as IAuthBaseResponse).accessToken
+                )
+              );
+
+              // retry the initial query
+              result = await baseQuery(args, api, extraOptions);
+            } else {
+              // api.dispatch(loggedOut())
+            }
+          } else {
+            // api.dispatch(loggedOut())
+          }
+        } finally {
+          // release must be called once the mutex should be released again.
+          release();
+        }
+      } else {
+        // wait until the mutex is available without locking it
+        await mutex.waitForUnlock();
+        result = await baseQuery(args, api, extraOptions);
+      }
+    } else {
+      notification.error({ message: "Произошла непредвиденная ошибка" });
+    }
   }
-  // if (result.error) {
-  //   if (result.error.status === 401) {
-  //     // checking whether the mutex is locked
-  //     if (!mutex.isLocked()) {
-  //       const release = await mutex.acquire();
-  //       try {
-  //         const refreshToken = getRefreshToken();
-
-  //         if (refreshToken) {
-  //           const refreshResult = await baseQuery(
-  //             {
-  //               url: "/auth/token",
-  //               body: {
-  //                 refreshToken,
-  //               },
-  //             },
-  //             api,
-  //             extraOptions
-  //           );
-  //           if (refreshResult.data) {
-  //             setRefreshToken(
-  //               (refreshResult.data as IAuthBaseResponse).refreshToken
-  //             );
-  //             api.dispatch(
-  //               setAccessToken(
-  //                 (refreshResult.data as IAuthBaseResponse).accessToken
-  //               )
-  //             );
-
-  //             // retry the initial query
-  //             result = await baseQuery(args, api, extraOptions);
-  //           } else {
-  //             // api.dispatch(loggedOut())
-  //           }
-  //         } else {
-  //           // api.dispatch(loggedOut())
-  //         }
-  //       } finally {
-  //         // release must be called once the mutex should be released again.
-  //         release();
-  //       }
-  //     } else {
-  //       // wait until the mutex is available without locking it
-  //       await mutex.waitForUnlock();
-  //       result = await baseQuery(args, api, extraOptions);
-  //     }
-  //   } else {
-  //     notification.error({ message: "Произошла непредвиденная ошибка" });
-  //   }
-  // }
   return result;
 };
